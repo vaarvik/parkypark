2c8001f492bf91cbf36550c71200cdd19d83f048
vaarvik <johannvaarvik@hotmail.com>
2020-10-08 20:29:49 +0200

    Added setter and getter for Parkinglot

--- src/main/java/parkypark/model/Parkinglot.java
+++ src/main/java/parkypark/model/Parkinglot.java
@@ -2,6 +2,23 @@ package parkypark.model;
 
 public class Parkinglot {
     private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
     private String address;
 
     public Parkinglot(String name, String address) {
