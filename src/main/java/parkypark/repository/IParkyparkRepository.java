d8db3f998235c0b48fa98e3bd88be1181372835b
eirikhc <71400720+eirikhc@users.noreply.github.com>
2020-10-10 12:18:01 +0200

    Updated interface and repository

--- src/main/java/parkypark/repository/IParkyparkRepository.java
+++ src/main/java/parkypark/repository/IParkyparkRepository.java
@@ -1,4 +1,12 @@
 package parkypark.repository;
 
+import parkypark.model.Parkinglot;
+
+import java.util.ArrayList;
+
 public interface IParkyparkRepository {
+    //hente alle parkeringsplasser, Arraylist foreløpig?
+    ArrayList<Parkinglot> getParkinglots();
+    //hente en spesifikk parkeringsplass
+    Parkinglot getParkinglot(String name);
 }
--- src/main/java/parkypark/repository/ParkyparkRepository.java
+++ src/main/java/parkypark/repository/ParkyparkRepository.java
@@ -1,4 +1,17 @@
 package parkypark.repository;
 
+import parkypark.model.Parkinglot;
+
+import java.util.ArrayList;
+
 public class ParkyparkRepository implements IParkyparkRepository {
+    @Override
+    public ArrayList<Parkinglot> getParkinglots() {
+        return null;
+    }
+
+    @Override
+    public Parkinglot getParkinglot(String name) {
+        return null;
+    }
 }
