ed0471d31115bee102a51d34cddcb06243b8a801
vaarvik <johannvaarvik@hotmail.com>
2020-10-21 18:44:59 +0200

    Web application set up with Javalin and JSON is sent into an url (api/parkinglots)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,20 @@
-
+import io.javalin.Javalin;
+import no.parkypark.repository.ParkyparkRepository;
 
 public class Main {
     public static void main(String[] args) {
+
+        //App startup
+        Javalin app = Javalin.create().start(7001);
+
+        //Allows us to use gradle type dependencies for web dependencies
+        app.config.enableWebjars();
+
+        //Pages
+        app.get("/", ctx -> ctx.html("<h1>Halla</h1>"));
+
+        //APIs
+        app.get("/api/parkinglots", ctx -> ctx.json(new ParkyparkRepository("src/main/resources/parkinglots.json")));
 
     }
 
