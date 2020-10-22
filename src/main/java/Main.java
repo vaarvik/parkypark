c05e7a7cd780dc389bd6dd443462e0167b1161dd
vaarvik <johannvaarvik@hotmail.com>
2020-10-22 18:58:02 +0200

    Added comments for what's needed for each page in frontend

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,6 +12,7 @@ public class Main {
 
             //Adds the stylesheet to the site
             config.addSinglePageRoot("/assets/styles/style", "vue/assets/styles/style.css");
+
         }).start(7001);
 
         /*
@@ -19,29 +20,56 @@ public class Main {
         ==========
          */
 
-        //shows all parkinglots
+        /*
+         * shows all parkinglots
+         * * Get title and picture of all parkinglots
+         */
         app.get("/", new VueComponent("index"));
 
-        //login page
+        /*
+         * login page
+         * * Get information that checks if user is signed in or not
+         */
         app.get("/login", new VueComponent("login"));
 
-        //shows a single parkinglot
+        /*
+         * shows a single parkinglot
+         * * Get all information about a single parkinglot
+         */
         app.get("/parkinglots/:parkinglotid", new VueComponent("parkinglot-handling/single-parkinglot"));
-//
-        //adds a parkinglot
+
+        /*
+         * adds a parkinglot
+         * * POST Request.
+         * * Send all information about the new parkinglot.
+         */
         app.get("/add-parkinglot", new VueComponent("parkinglot-handling/add-parkinglot"));
-//
-        //shows all parkinglots related to a single user
+
+        /*
+         * shows all parkinglots related to a single user
+         * Get all parkingslots for a specific user
+         */
         app.get("/user/:userid/parkinglots", new VueComponent("parkinglot-handling/user-parkinglots"));
 
-        //edits a specific parkinglot for a logged in user
+        /*
+         * edits a specific parkinglot for a logged in user
+         * POST Request.
+         * Update information about a parkinglot
+         */
         app.get("/user/:userid/parkinglot/:parkinglotid/edit", new VueComponent("parkinglot-handling/edit-parkinglot"));
 
-        //site for booking a specific parkinglot
+        /*
+         * site for booking a specific parkinglot
+         * POST Request.
+         * Send all information about the new booking.
+         */
         app.get("/parkinglot/:parkinglotid/book", new VueComponent("parkinglot-handling/book-parkinglot"));
 
         //APIs
-        app.get("/api/parkinglots", ctx -> ctx.json(new ParkyparkRepository("src/main/resources/data/parkinglots.json")));
+        app.get("/api/parkinglots", ctx ->
+                ctx.json(new ParkyparkRepository("src/main/resources/data/parkinglots.json").getAllParkinglots()
+                )
+        );
 
     }
 
