ff224df0f2db895519f7ceb5022e22ff53e5367a
sigridig <siggjo@icloud.com>
2020-11-05 15:58:49 +0100

    fixed frontend for updateParkinglot

--- src/main/resources/vue/view/parkinglot-handling/edit-parkinglot.vue
+++ src/main/resources/vue/view/parkinglot-handling/edit-parkinglot.vue
@@ -1,20 +1,20 @@
 <template id="edit-parkinglot">
     <div>
-        <h1>{{parkinglot.name}}</h1>
+        <h1>{{ title }}</h1>
         <p>Endre parkeringplassen ved å fylle inn feltene nedenfor.</p>
         <hr>
-        <form @submit="checkForm" :action="`/api/parkinglots/${parkinglot.id}/edit`" method="POST">
+        <form @submit="onSubmit">
             <div class="field input">
                 <label for="input-name">Tittel</label>
-                <input type="text" name="name" id="input-name" placeholder="Tittel..." :value="parkinglot.name">
+                <input type="text" name="name" id="input-name" placeholder="Tittel..." v-model="parkinglot.name">
             </div>
             <div class="field input">
                 <label for="input-address">Adresse</label>
-                <input type="text" name="address" id="input-address" placeholder="Tittel..." :value="parkinglot.address">
+                <input type="text" name="address" id="input-address" placeholder="Tittel..." v-model="parkinglot.address">
             </div>
             <div class="field input">
                 <label for="input-price">Pris</label>
-                <input type="text" name="price" id="input-price" placeholder="Tittel..." :value="parkinglot.price">
+                <input type="text" name="price" id="input-price" placeholder="Tittel..." v-model="parkinglot.price">
             </div>
             <input type="hidden" name="id" :value="parkinglot.id">
             <input type="hidden" name="userid" :value="parkinglot.userid">
@@ -26,18 +26,27 @@
     Vue.component("edit-parkinglot", {
         template: "#edit-parkinglot",
         data: () => ({
-            parkinglot: {}
+            parkinglot: {},
+            title: '',
         }),
         created(){
             fetch(`/api/parkinglots/${this.$javalin.pathParams["parkinglotid"]}`)
                 .then(res => res.json())
-                .then(res => this.parkinglot = res)
+                .then(res => {
+                  this.title = res.name;
+                  this.parkinglot = res;
+                })
                 .catch(() => alert("Error while fetching parkinglot"));
         },
         methods:{
-            checkForm:function(e) {
-
-            }
+          onSubmit(event) {
+            event.preventDefault();
+            fetch('/api/parkinglots/:parkinglotid/edit', {
+              method: 'POST',
+              body: JSON.stringify(this.parkinglot),
+            });
+          },
+          checkForm:function(e) {}
         }
     });
 </script>
