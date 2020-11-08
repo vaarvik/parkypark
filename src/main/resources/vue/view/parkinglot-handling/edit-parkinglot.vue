c71d32f74964562352bef5739b1cb71801918bb8
vaarvik <johannvaarvik@hotmail.com>
2020-11-08 21:55:50 +0100

    Vue: Created layout and design for editing a parkinglot

--- src/main/resources/vue/view/parkinglot-handling/edit-parkinglot.vue
+++ src/main/resources/vue/view/parkinglot-handling/edit-parkinglot.vue
@@ -1,25 +1,65 @@
 <template id="edit-parkinglot">
-    <div>
-        <h1>{{ title }}</h1>
-        <p>Endre parkeringplassen ved å fylle inn feltene nedenfor.</p>
-        <hr>
-        <form @submit="onSubmit">
-            <div class="field input">
-                <label for="input-name">Tittel</label>
-                <input type="text" name="name" id="input-name" placeholder="Tittel..." v-model="parkinglot.name">
+<div class="site-wrapper">
+        <header class="site-header">
+            <a href="/">
+                <h2 class="site-branding">ParkyPark</h2>
+            </a>
+            <div class="site-navigation">
+                <a href="/parkinglots/add">Add parkinglot</a>
+                <a href="">Your parkinglots</a>
+                <a href="">Log out</a>
             </div>
-            <div class="field input">
-                <label for="input-address">Adresse</label>
-                <input type="text" name="address" id="input-address" placeholder="Tittel..." v-model="parkinglot.address">
+        </header>
+        <main class="site-content">
+            <div class="entry">
+                <header class="entry-header">
+                    <h1 class="entry-header__heading">{{ title }}</h1>
+                    <p>Endre parkeringplassen ved å fylle endre feltene nedenfor.</p>
+                </header>
+                <div class="entry-info">
+                    <div class="entry-info__text">
+                        <form @submit="onSubmit">
+                            <label for="name">Navn</label>
+                            <input class="field input" type="text" name="name" id="name" v-model="parkinglot.name">
+                            <br>
+
+                            <label for="address">Adresse</label>
+                            <input class="field input" type="text" name="address" id="address" v-model="parkinglot.address">
+                            <br>
+
+                            <label for="price">Pris</label>
+                            <input class="field input" type="number" step="1" name="price" id="price" v-model="parkinglot.price" min="0">
+                            <br>
+
+                            <label for="image">Bilde</label>
+                            <input placeholder="Bildeurl..." class="field input" type="url" name="image" id="image" v-model="parkinglot.image">
+                            <br>
+
+                            <label for="checkin">Check in</label>
+                            <input class="field input" type="date" :min="getToday()" name="checkin" id="checkin" v-model="parkinglot.checkin">
+                            <br>
+
+                            <label for="checkout">Check out</label>
+                            <input class="field input" type="date" :min="parkinglot.checkin ? parkinglot.checkin : getToday()" name="checkout" id="checkout" v-model="parkinglot.checkout">
+                            <br>
+
+                            <label for="description">Beskrivelse</label>
+                            <textarea class="field" name="description" id="description" cols="30" rows="2" v-model="parkinglot.description"></textarea>
+                            <br>
+
+                            <button class="btn">Endre parkeringsplass</button>
+                        </form>
+                    </div>
+                    <img v-if="parkinglot.image" class="entry-info__image" :src="parkinglot.image" alt="">
+                </div>
             </div>
-            <div class="field input">
-                <label for="input-price">Pris</label>
-                <input type="text" name="price" id="input-price" placeholder="Tittel..." v-model="parkinglot.price">
+        </main>
+        <footer class="site-footer">
+            <div class="site-footer__content">
+                <p>© ParkyPark 2020</p>
+                <a href="mailto:contact@parkypark.no">Kontakt oss</a>
             </div>
-            <input type="hidden" name="id" :value="parkinglot.id">
-            <input type="hidden" name="userid" :value="parkinglot.userid">
-            <input type="submit" value="Submit">
-        </form>
+        </footer>
     </div>
 </template>
 <script>
@@ -39,14 +79,23 @@
                 .catch(() => alert("Error while fetching parkinglot"));
         },
         methods:{
-          onSubmit(event) {
-            event.preventDefault();
-            fetch('/api/parkinglots/:parkinglotid/edit', {
-              method: 'POST',
-              body: JSON.stringify(this.parkinglot),
-            });
-          },
-          checkForm:function(e) {}
+            onSubmit(event) {
+                event.preventDefault();
+                fetch('/api/parkinglots/:parkinglotid/edit', {
+                method: 'POST',
+                body: JSON.stringify(this.parkinglot),
+                });
+            },
+            checkForm:function(e) {
+
+            },
+            getToday() {
+                let today = new Date();
+                let dd = String(today.getDate()).padStart(2, '0');
+                let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+                let yyyy = today.getFullYear();
+                return `${yyyy}-${mm}-${dd}`;
+            }
         }
     });
 </script>
