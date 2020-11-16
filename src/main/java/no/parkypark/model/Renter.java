c6f6b00536d4ad5e6a401d7c02853954b9c8731a
vaarvik <johannvaarvik@hotmail.com>
2020-11-16 19:18:42 +0100

    Removed Car class and carId from Renter

--- src/main/java/no/parkypark/model/Renter.java
+++ src/main/java/no/parkypark/model/Renter.java
@@ -3,10 +3,8 @@ package no.parkypark.model;
 import java.util.UUID;
 
 public class Renter extends User {
-    private UUID carId;
 
-    public Renter(UUID userId, String name, String email, String password, UUID carId) {
+    public Renter(UUID userId, String name, String email, String password) {
         super(userId, name, email, password);
-        this.carId = carId;
     }
 }
