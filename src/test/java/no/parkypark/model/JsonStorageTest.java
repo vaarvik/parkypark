62381b1e960f8e63396df8f5d61970529ff41d16
vaarvik <johannvaarvik@hotmail.com>
2020-11-23 19:58:35 +0100

    Updated readFromFileTest in JSON Storage Tests so it tests an entire array of parkinglots instead of some specific elements in it and its size.

--- src/test/java/no/parkypark/model/JsonStorageTest.java
+++ src/test/java/no/parkypark/model/JsonStorageTest.java
@@ -1,18 +1,31 @@
 package no.parkypark.model;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
-import org.junit.jupiter.api.io.TempDir;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class JsonStorageTest {
-	private static final String JSON_PARKINGLOT_FILE = "[{ \"name\" : \"Best Spot!\", \"address\" : \"Planetroad 2\",\"id\" : \"b5319909-1968-442c-93e9-fd08d24d985f\",\"ownerId\" : \"fe34e136-ff5a-4c2f-93c3-1820bc170f83\",\"price\" : 50.0}]";
+	private static final String JSON_PARKINGLOT_FILE =
+			"[" +
+				"{ \"name\" : \"Oscars Parkeringsplass\", \"address\" : \"Oscarveien 14\",\"id\" : \"b5319909-1968-442c-93e9-fd08d24d985f\",\"ownerId\" : \"4\",\"price\" : 20.0}," +
+				"{ \"name\" : \"Kristoffers Parkeringsplass\", \"address\" : \"Kristofferveien 14\",\"id\" : \"e32909-1968-442c-93e9-f343d985f\",\"ownerId\" : \"6\",\"price\" : 50.0}" +
+			"]";
+
+	private ArrayList<Parkinglot> expectedLots = new ArrayList<>();
+
+	@BeforeEach
+	void setUp() {
+		expectedLots.add(new Parkinglot("Oscars Parkeringsplass", "Oscarveien 14", "4", 20));
+		expectedLots.add(new Parkinglot("Kristoffers Parkeringsplass", "Kristofferveien 14", "6", 50));
+	}
 
 	@Test
 	void readFromFileTest(@TempDir Path tempDir) throws IOException {
@@ -22,15 +35,11 @@ public class JsonStorageTest {
 		JsonStorage<Parkinglot> parkinglotJsonStorage = new JsonStorage<>(Parkinglot.class, path.toAbsolutePath().toString());
 		List<Parkinglot> parkinglotsFromFile = parkinglotJsonStorage.read();
 
-		assertEquals(1, parkinglotsFromFile.size());
+		for(int i = 0; i < expectedLots.size(); i++){
+			expectedLots.get(i).setId(parkinglotsFromFile.get(i).getId());
+		}
 
-		Parkinglot lot = parkinglotsFromFile.get(0);
-
-		assertEquals(50.0, lot.getPrice());
-		assertEquals("Planetroad 2", lot.getAddress());
-		assertEquals("Best Spot!", lot.getName());
-		assertEquals("b5319909-1968-442c-93e9-fd08d24d985f", lot.getId());
-		assertEquals("fe34e136-ff5a-4c2f-93c3-1820bc170f83", lot.getOwnerId());
+		assertEquals(parkinglotsFromFile, expectedLots);
 	}
 
 	@Test
@@ -39,7 +48,7 @@ public class JsonStorageTest {
 
 		JsonStorage<Parkinglot> parkinglotJsonStorage = new JsonStorage<>(Parkinglot.class, path.toAbsolutePath().toString());
 
-		Parkinglot lot = new Parkinglot("Name", "Adress", "OwnerId", 100);
+		Parkinglot lot = new Parkinglot("Sigveien 14", "Sigveien 14, Trondheim", "14", 100);
 		ArrayList<Parkinglot> lots = new ArrayList<>();
 		lots.add(lot);
 
